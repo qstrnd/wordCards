36174a7cac0fa594d61edf74a3dd1f2e0d9f655e
Andy <a.iakovlev@proton.me>
2024-12-08 16:38:29 +0100

    Add multiple presentation destinations

--- WordCards/Features/Composable/Contacts/ContactsFeature.swift
+++ WordCards/Features/Composable/Contacts/ContactsFeature.swift
@@ -12,35 +12,62 @@ struct Contact: Equatable, Identifiable {
 struct ContactsFeature {
     @ObservableState
     struct State: Equatable {
-        @Presents var addContact: AddContactFeature.State?
+        @Presents var destination: Destination.State?
         var contacts: IdentifiedArrayOf<Contact> = []
     }
 
     enum Action {
         case addButtonTapped
-        case addContact(PresentationAction<AddContactFeature.Action>)
+        case deleteButtonTapped(id: Contact.ID)
+        case destination(PresentationAction<Destination.Action>)
+
+        enum Alert: Equatable {
+            case confirmDeletion(id: Contact.ID)
+        }
+    }
+
+    @Reducer
+    enum Destination {
+        case addContact(AddContactFeature)
+        case alert(AlertState<ContactsFeature.Action.Alert>)
     }
 
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .addButtonTapped:
-                state.addContact = AddContactFeature.State(
-                    contact: Contact(id: UUID(), name: "")
+                state.destination = .addContact(
+                    AddContactFeature.State(
+                        contact: Contact(id: UUID(), name: "")
+                    )
                 )
                 return .none
-            case let .addContact(.presented(.delegate(.saveContact(contact)))):
+            case let .destination(.presented(.addContact(.delegate(.saveContact(contact))))):
                 state.contacts.append(contact)
                 return .none
-            case .addContact:
+            case let .destination(.presented(.alert(.confirmDeletion(id: id)))):
+                state.contacts.remove(id: id)
+                return .none
+            case .destination:
+                return .none
+            case let .deleteButtonTapped(id: id):
+                state.destination = .alert(
+                    AlertState {
+                        TextState("Are you sure?")
+                    } actions: {
+                        ButtonState(role: .destructive, action: .confirmDeletion(id: id)) {
+                            TextState("Delete")
+                        }
+                    }
+                )
                 return .none
             }
         }
-        .ifLet(\.$addContact, action: \.addContact) {
-            AddContactFeature()
-        }
+        .ifLet(\.$destination, action: \.destination)
     }
 }
+
+extension ContactsFeature.Destination.State: Equatable {}
 
 struct ContactsView: View {
     @Bindable var store: StoreOf<ContactsFeature>
@@ -49,7 +76,16 @@ struct ContactsView: View {
         NavigationStack {
             List {
                 ForEach(store.contacts) { contact in
-                    Text(contact.name)
+                    HStack {
+                        Text(contact.name)
+                        Spacer()
+                        Button {
+                            store.send(.deleteButtonTapped(id: contact.id))
+                        } label: {
+                            Image(systemName: "trash")
+                                .foregroundColor(.red)
+                        }
+                    }
                 }
             }
             .navigationTitle("Contacts")
@@ -63,12 +99,13 @@ struct ContactsView: View {
                 }
             }
             .sheet(
-                item: $store.scope(state: \.addContact, action: \.addContact)
+                item: $store.scope(state: \.destination?.addContact, action: \.destination.addContact)
             ) { addContactStore in
                 NavigationStack {
                     AddContactView(store: addContactStore)
                 }
             }
+            .alert($store.scope(state: \.destination?.alert, action: \.destination.alert))
         }
     }
 }
